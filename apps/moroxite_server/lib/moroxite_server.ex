508717440092ac2e3c1aef214e37f5118bb35122
Nikolay Kotsev <nkotsev@nikolay.kotsev@code-runners.com>
2017-07-05 14:33:43 +0300

    Remove the example hello world function

--- apps/moroxite_server/lib/moroxite_server.ex
+++ apps/moroxite_server/lib/moroxite_server.ex
@@ -2,17 +2,4 @@ defmodule MoroxiteSever do
   @moduledoc """
   Documentation for MoroxiteSever.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MoroxiteSever.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
